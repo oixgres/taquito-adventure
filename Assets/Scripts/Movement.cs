4201e9d0a27321474d20b29e7d4927ba61090807
oixgres <a1252819@uabc.edu.mx>
2022-05-14 23:40:08 -0700

    Added movement to the camera

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,12 +6,6 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
